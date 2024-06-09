06efdc6962203ffd56e3f1c1e6a2a1573d1ea941
nicoosk <nicolas.hui18@gmail.com>
2024-06-08 22:17:56 -0400

    Proyecto actualizado

---
+++ js/index.js
@@ -0,0 +1,44 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.titulo', {
+        duration: 3000,
+        origin: 'bottom',
+        distance:'1200px'
+    });
+
+    sr.reveal('#div3', {
+        duration: 3000,
+        origin: 'top',
+        distance:'100px'
+    });
+
+    sr.reveal('.desc', {
+        duration: 4000,
+        origin:'bottom',
+        distance:'100px'
+    });
+    
+    sr.reveal('.resources', {
+        duration: 3000,
+        origin:'bottom',
+        distance:'100px'
+    });
+
+    sr.reveal('#div4 ul', {
+        duration: 4000,
+        origin:'bottom',
+        distance:'100px'
+    });
+
+    sr.reveal('.iframe-container', {
+        duration: 4000,
+        origin:'bottom',
+        distance:'100px'
+    });
+
+    sr.reveal('.iframe-container2', {
+        duration: 4000,
+        origin:'bottom',
+        distance:'100px'
+    });
+    
